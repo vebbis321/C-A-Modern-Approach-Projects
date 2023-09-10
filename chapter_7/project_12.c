8b9dd47405236cce37c9908be116297de2ac3eeb
vebbis321 <veb.dan@hotmail.com>
2023-09-10 11:34:16 +0200

    Finished project: 12

--- chapter_7/project_12.c
+++ chapter_7/project_12.c
@@ -4,14 +4,22 @@ float calculate(float f, float new_f, char operand);
 
 int main(void)
 {
-    float f = 0;
-    char ch, operand;
+    float f = 0, new_f;
+    char ch = '+';
 
-    while ((ch = getchar()) != '\n') {
-        if (ch >= 48 && ch <= 57) {
-            f = calculate(f, ch - 48, '+');
-        } else if 
-    }
+
+    do {
+        scanf(" %f", &new_f);
+        
+        switch (ch) {
+            case '+': case '-': case '/': case '*':
+                f = calculate(f, new_f, ch);
+        }
+        printf("%f\n", f);
+
+    } while ((ch = getchar()) != '\n');
+    
+    printf("JE: %f", f);
 }
 
 float calculate(float f, float new_f, char operand)
