2460a3b7ce6563e60c51cadb79d0079e279aac09
vebbis321 <veb.dan@hotmail.com>
2023-09-12 09:53:52 +0200

    Finished project: 14

--- chapter_7/project_14.c
+++ chapter_7/project_14.c
@@ -1,8 +1,21 @@
 #import <stdio.h>
+#import <math.h>
 
 int main(void)
 {
-    double x;
+    double x, y, average = 1;
     printf("Enter a positive number: ");
     scanf("%lf", &x);
+
+    do { 
+        y = average;
+
+        double division = x / y;
+        average = ((y + division) / 2);
+
+        printf("%lf\t%lf\t%lf\t%lf\n", x, y, division, average);
+
+    } while (fabs(y - average) > 0.00001);
+
+    return 0;
 }
