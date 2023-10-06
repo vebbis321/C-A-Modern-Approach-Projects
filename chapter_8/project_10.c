daf8ab7689ef5be4734609ded1a4baf9fb7a8235
vebbis321 <veb.dan@hotmail.com>
2023-10-06 21:22:37 +0200

    finished project 10

--- chapter_8/project_10.c
+++ chapter_8/project_10.c
@@ -17,29 +17,22 @@ int main(void)
         hour,
         minute,
         closest_time,
-        index, 
-    min = 0, max = departure_times[7];
+        index = 0;
       
     printf("Enter a 24-hour time: ");
     scanf("%d :%d", &hour, &minute);
     user_time = hour * 60 + minute;
 
-
-    if (user_time <= departure_times[0] || user_time > departure_times[7]) {
-        closest_time = departure_times[0];
-    }
-
     for (index = 0; index < 8; index++) {
-
-
-        if (index != 7) {
-            if (departure_times[index] < user_time && user_time < departure_times[index + 1]) {
-                closest_time = departure_times[index];
-            } 
-        }
+        if (index == 7) break;
+            
+        if (user_time <= departure_times[index] + (departure_times[index + 1] - departure_times[index]) / 2)
+            break;
     }
 
-    printf("Closest departure time is %d", closest_time);
+
+    printf("Closest departure time today is %d\n", departure_times[index]);
+    printf("Arrival time is %d\n", arrival_times[index]);
 
     return 0;
 }
