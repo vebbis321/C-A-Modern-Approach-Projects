22ce43363fb8903f0824ebd976db866b9440e041
vebbis321 <veb.dan@hotmail.com>
2023-10-12 17:19:55 +0200

    finished project 14

---
+++ chapter_8/project_14.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    char arr[100];
+    char terminating_char, ch;
+    int index = 0;
+
+    printf("Enter a sentence: ");
+
+    while ((ch = getchar()) != '\n') {
+        if (ch == '.' || ch == '?' || ch == '!') {
+            terminating_char = ch;
+            break;
+        }
+        else {
+            arr[index] = ch;
+            index++;
+        }
+    }
+
+    printf("Reversal of sentence: ");
+
+    int temp_index = 0;
+    char temp_arr[20];
+
+    for (int i = index - 1; i >= 0; i--) {
+        temp_arr[temp_index] = arr[i];
+        temp_index++;
+
+        if (arr[i] == ' ' || i == 0) {
+            for (int n = temp_index - 1; n >= 0; n--) {
+                if (temp_arr[n] != ' ') {
+                    printf("%c", temp_arr[n]);
+                }
+            }
+            temp_index = 0;
+            if (i != 0) {
+                printf(" ");
+            }
+        }
+    }
+
+    printf("%c\n", terminating_char);
+
+    return 0;
+}
