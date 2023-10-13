e316b5b7142ff1c739a7a5d99c8cf44ce2ac374b
vebbis321 <veb.dan@hotmail.com>
2023-10-13 18:40:35 +0200

    finished project 15

--- chapter_8/project_15.c
+++ chapter_8/project_15.c
@@ -1,52 +1,41 @@
 #include <ctype.h>
 #include <stdio.h>
 
+#define MAX_VALUE 50
+
 int main(void)
 {
     char ch, cryptic_char;
-    char arr[100] = {0};
+    char arr[MAX_VALUE] = {0};
     int shift_amount;
-    int idx = 0;
+    int i, length;
 
     printf("Enter message to be encrypted: ");
-
-    while ((ch = getchar()) != '\n') {
-        arr[idx] = ch;
-        idx++;
+    for (i = 0, length = 0; (ch = getchar()) != '\n' && i < MAX_VALUE; i++) {
+        length++;
+        arr[i] = ch;
     }
 
     printf("Enter shift amount: ");
     scanf("%d", &shift_amount);
 
     printf("Encrypted message: ");
-
-    for (int i = 0; i < idx; i++) {
-        printf("%c\n", arr[i]);
+    for (i = 0; i < length; i++) {
         // from a to z
-        if (arr[i] >= 'a' || arr[i] <= 'z') {
+        if (arr[i] >= 'a' && arr[i] <= 'z') {
             // if shift_amount added with char is bigger than z
-            if ((arr[i] + shift_amount) > 122) {
-                cryptic_char = ((arr[i] + shift_amount) - 122) + 61;
-            }
-            else {
-                cryptic_char = arr[i] + shift_amount;
-            }
+            cryptic_char = ((arr[i] - 'a') + 3) % 26 + 'a';
         }
         // from A to Z
-        else if (arr[i] >= 'A' || arr[i] <= 'Z') {
+        else if (arr[i] >= 'A' && arr[i] <= 'Z') {
             // if shift_amount added with char is bigger than Z
-            if ((arr[i] + shift_amount) > 90) {
-                cryptic_char = ((arr[i] + shift_amount) - 122) + 61;
-            }
-            else {
-                cryptic_char = arr[i] + shift_amount;
-            }
+            cryptic_char = ((arr[i] - 'A') + 3) % 26 + 'A';
         }
         else {
             cryptic_char = arr[i];
         }
 
-        printf("%ch", cryptic_char);
+        putchar(cryptic_char);
     }
 
     return 0;
